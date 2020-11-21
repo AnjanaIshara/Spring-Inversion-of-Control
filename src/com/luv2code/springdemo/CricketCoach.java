f455ff153239e3db01e1cb74b160d5bd89de3f44
anjana ishara <anjanaishara08@gmail.com>
2020-11-21 23:05:06 +0530

    added an additional class

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Try to rotate the sides";
+    }
+}
